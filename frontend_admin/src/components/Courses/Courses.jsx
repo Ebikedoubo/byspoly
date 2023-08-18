17df843a515f381c4ab15e5b8024bf180441bfbe
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-08-18 14:55:30 +0400

    feat: Add new pages

---
+++ frontend_admin/src/components/Courses/Courses.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Courses() {
+  return (
+      <div className="mt-12 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">Courses</div>
+  )
+}
+
+export default Courses
