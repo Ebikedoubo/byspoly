d09629bb747e6b736231dc8d0e53df589afa4a9a
edwin <edwinosayande@gmail.com>
2023-09-03 22:55:05 +0100

    student Loginss

--- frontend_student/src/routes/authRoutes.js
+++ frontend_student/src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import Login from "../pages/auth/Login";
+import StudentEnrollment from "../components/StudentEnrollment";
 
 export const authRoutes = [
   {
     path: '/',
     component: Login,
+    exact: true,
+  },
+  {
+    path: '/studentenrollment',
+    component: StudentEnrollment,
     exact: true,
   },
 ];
