a1d5924a1d726bc5d786c3de7743a5b7e3b2626e
kingsley achumie <kingsonly13c@gmail.com>
2023-10-06 22:20:42 +0100

    wip

--- frontend_student/src/services/urls.js
+++ frontend_student/src/services/urls.js
@@ -1,3 +1,7 @@
 export const baseUrl = process.env.REACT_APP_API_URL;
 export const stateUrl = `${baseUrl}/site/state`
 export const localGovtUrl = `${baseUrl}/site/local-government`
+export const facultyUrl = `${baseUrl}/faculty`
+export const departmentUrl = `${baseUrl}/faculty/faculty-departments`
+export const examTypeUrl = `${baseUrl}/exam-type`
+export const currentApplicationFeeUrl = `${baseUrl}/fee/current-application-fee`
